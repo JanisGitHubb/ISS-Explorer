e38d28177b9d787b9d4749afbc97a24ad830a3f4
bremmersg <bremmers.gustavs@gmail.com>
2023-12-19 22:31:17 +0200

    API datu apstrāde

---
+++ app.py
@@ -0,0 +1,37 @@
+import requests
+import time
+import csv
+
+# with open('data.csv', mode='w') as csvfile:
+#         fieldnames = ['number', 'timestamp', 'longitude', 'latitude']
+#         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+#         writer.writeheader()
+#         csvfile.close()
+
+file = open('data.csv', 'a', newline='')
+datawriter = csv.writer(file)
+
+# pagaidu skaitītājs, kas palīdz saprast datu plūsmu.
+n = 1
+for i in range(5):
+    response = requests.get('http://api.open-notify.org/iss-now.json')
+
+    status = response.status_code
+    message = response.json()['message']
+    timestamp = response.json()['timestamp']
+    longitude = response.json()['iss_position']['longitude']
+    latitude = response.json()['iss_position']['latitude']
+
+    print(status)
+    print(message)
+    print(timestamp)
+    print(longitude)
+    print(latitude)
+    print("----------")
+    print(response.json())
+
+    datawriter.writerow([n, timestamp, longitude, latitude])
+
+
+    time.sleep(2)
+    n = n+1
